f939f0b3eefca45e7d4a25441d5a27a18ca7bc91
s31360 <s31360@pjwstk.edu.pl>
2025-11-23 16:08:08 +0100

    Implemented Factory pattern

--- DesignPattern/Factory/DatabaseFactory.cs
+++ DesignPattern/Factory/DatabaseFactory.cs
@@ -2,9 +2,19 @@
 {
     public static class DatabaseFactory
     {
-        //public static IDatabase CreateDatabase(...)
-        //{
-        //  ...
-        //}
+        public static IDatabase CreateDatabase(DatabaseType type)
+        {
+            switch (type)
+            {
+                case DatabaseType.SqlServer:
+                    return new SqlServerDatabase();
+
+                case DatabaseType.Oracle:
+                    return new OracleDatabase();
+
+                default:
+                    throw new System.ArgumentException("Unknown database type", nameof(type));
+            }
+        }
     }
 }
