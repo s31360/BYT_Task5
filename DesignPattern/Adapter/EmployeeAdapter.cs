8dedf1b5459a7b43c15fdd9f0dc2d7571b4aa63f
s31360 <s31360@pjwstk.edu.pl>
2025-11-23 16:12:08 +0100

    Implemented Adapter pattern

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -5,9 +5,24 @@ namespace DesignPattern.Adapter
     {
         private readonly BillingSystem thirdPartyBillingSystem = new();
 
-        //public void ProcessCompanySalary(...)
-        //{
-        //    ...
-        //}
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            var employeeList = new List<Employee>();
+
+            int rows = employeesArray.GetLength(0);
+
+            for (int i = 0; i < rows; i++)
+            {
+                int id = int.Parse(employeesArray[i, 0]);
+                string name = employeesArray[i, 1];
+                string designation = employeesArray[i, 2];
+                decimal salary = decimal.Parse(employeesArray[i, 3]);
+
+                var employee = new Employee(id, name, designation, salary);
+                employeeList.Add(employee);
+            }
+
+            thirdPartyBillingSystem.ProcessSalary(employeeList);
+        }
     }
 }
